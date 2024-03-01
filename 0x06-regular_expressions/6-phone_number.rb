8b12ef043f92528de1ecc4a45974425d65f1d978
ezzkessem8 <92451799+ezzkessem8@users.noreply.github.com>
2024-03-01 19:45:51 +0300

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,11 +1,2 @@
 #!/usr/bin/env ruby
-
-phone_number = ARGV[0]
-
-# Remove any non-digit characters from the input
-phone_number.gsub!(/\D/, '')
-
-# Check if the phone number is exactly 10 digits long
-if phone_number.length == 10
-  puts phone_number
-end
+puts ARGV[0].scan(/^\d{10}$/).join
